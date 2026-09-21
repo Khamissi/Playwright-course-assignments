9d1e6c70cb507e4022356dc3e99f39690828734e
Ahmed Elkhamisy <ahmedelkhamissi@yahoo.com>
2026-09-21 13:53:23 +0300

    Fix PR review comments

--- tests/test-1.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://rahulshettyacademy.com/angularpractice/shop');
-  await page.locator('app-card').filter({ hasText: 'Blackberry $24.99 Lorem ipsum' }).getByRole('button').click();
-  await page.getByText('Checkout ( 1 ) (current)').click();
-});
